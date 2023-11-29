a6390170d8117e96c0923752e53c976713c8807a
SurKaa <98200894+surkaa@users.noreply.github.com>
2023-11-29 22:55:44 +0800

    Create TestComponent.vue

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import type {HeaderMenu} from "@/modules/HeaderMenu.ts";
+import {ref} from "vue";
+
+const menu = ref<HeaderMenu>({
+  id: 0,
+  text: 'string',
+  target: 'string',
+  isOthersLink: false,
+})
+
+</script>
+
+<template>
+  <div class="test-component">
+    <h1>This is a test component</h1>
+    <p>{{ menu }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
